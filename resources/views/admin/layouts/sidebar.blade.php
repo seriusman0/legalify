2554a8ff2e1f2960314fe661e8532819c8c755e3
Seriusman Waruwu <61109185+seriusman0@users.noreply.github.com>
2025-04-30 13:08:41 +0700

    update avatar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('admin.index') }}" class="brand-link">
-      <img src="{{ asset('assets/icons/legalifylogoblack.png') }}" alt="Legalify Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{ asset('assets/icons/logo1.jpeg') }}" alt="Legalify Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Legalify</span>
     </a>
 
